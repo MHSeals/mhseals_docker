af212359d29709bccd500352ca89c786ff68aa4c
1unarzDev <lunarzDev@outlook.com>
2025-09-19 17:35:39 -0500

    Fix installation script

--- install_docker.sh
+++ install_docker.sh
@@ -9,6 +9,25 @@ echo "Determining Linux distro"
 if [[ "$ID" == "arch" || "$ID_LIKE" == *"arch"* ]]; then
     echo "Arch-based distro Docker installation running..."
     sudo pacman -Syu docker
+elif [[ "$ID" == "ubuntu" ]]; then
+    echo "Ubuntu Docker installation running..."
+    
+    # Add Docker's official GPG key:
+    sudo apt-get update
+    sudo apt-get install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+    
+    # Add the repository to Apt sources:
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+
+    # Install latest version
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 elif [[ "$ID" == "debian" || "$ID_LIKE" == *"debian"* ]]; then
     echo "Debian-based distro Docker installation running..."
     
