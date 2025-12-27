cb78653d5ea1cd53bf171aaa58f81a1efb2f1d78
1unarzDev <lunarzDev@outlook.com>
2025-12-27 00:25:01 -0600

    please

--- .devcontainer/package_clean.sh
+++ .devcontainer/package_clean.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+
+apt-get remove --purge opencv-dev
+apt-get autoremove
 rm -rf /usr/local/include/opencv4
 rm -rf /usr/local/lib/libopencv_*
 rm -rf /usr/local/lib/python3*/dist-packages/cv2
