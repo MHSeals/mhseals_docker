4ddc17909df816be715c6e2c24d3f06fbadca633
1unarzDev <lunarzDev@outlook.com>
2026-01-04 16:48:49 -0600

    remove hjson pip insatll

--- setup.linux.sh
+++ setup.linux.sh
@@ -47,7 +47,6 @@ elif [[ "$ID" == "ubuntu" || "$ID" == "debian" || "$ID_LIKE" == *"debian"* ]]; t
     # Install prerequisites
     sudo apt-get update
     sudo apt-get install -y ca-certificates curl gnupg lsb-release python3-pip
-    pip install hjson
 
     # Add Docker repository
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(. /etc/os-release; echo "$VERSION_CODENAME") stable" | \
