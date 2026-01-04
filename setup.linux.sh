7ef21bce9b373ad440838d3b3084d43ca7cd57e7
1unarzDev <lunarzDev@outlook.com>
2026-01-04 15:16:28 -0600

    Add conditional nvidia container toolkit installation

--- setup.linux.sh
+++ setup.linux.sh
@@ -25,7 +25,11 @@ if [[ "$ID" == "arch" || "$ID_LIKE" == *"arch"* ]]; then
     for pkg in docker.io docker-doc podman-docker containerd runc; do yay -Rns $pkg; done
 
     echo "Installing all required packages..."
-    yay -S --noconfirm docker docker-buildx xorg-xwayland visual-studio-code-bin python-hjson nvidia-container-toolkit
+    yay -S --noconfirm docker docker-buildx xorg-xwayland visual-studio-code-bin python-hjson
+
+    if [ "$(uname -m)" = "aarch64" ]; then
+        yay -S --noconfirm nvidia-container-toolkit
+    fi
 elif [[ "$ID" == "ubuntu" || "$ID" == "debian" || "$ID_LIKE" == *"debian"* ]]; then
     echo "Debian-based distro installation running..."
 
@@ -67,13 +71,15 @@ elif [[ "$ID" == "ubuntu" || "$ID" == "debian" || "$ID_LIKE" == *"debian"* ]]; t
     sudo apt update
     sudo apt install code
 
-    echo "Installing NVIDIA container tools..."
-    curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
-    curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
-        sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
-        sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
-    
-    sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
+    if [ "$(uname -m)" = "aarch64" ]; then
+        echo "Installing NVIDIA container tools..."
+        curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+        curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+            sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+            sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+        
+        sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
+    fi
 else
     echo "Installation failed!"
     echo "Unsupported distro: $ID"
