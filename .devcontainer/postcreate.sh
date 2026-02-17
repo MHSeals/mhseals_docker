4343711e08d5f53d2690eeac2945ca9ac1a05c3d
evandestafeno <evan.destafeno@outlook.com>
2026-02-17 07:11:56 +0000

    postcreate geographlib install fix

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -14,7 +14,10 @@ echo "[postcreate] Updating rosdep..."
 rosdep update
 
 echo "[postcreate] Install geoid dataset for mavros..."
-sudo geographiclib-get-geoids egm96-5
+#sudo geographiclib-get-geoids egm96-5
+sudo wget -O /tmp/egm96-5.tar.bz2 'https://psychz.dl.sourceforge.net/project/geographiclib/geoids-distrib/egm96-5.tar.bz2?viasf=1' #this link came to me in a dream
+sudo tar -xf /tmp/egm96-5.tar.bz2 -C /usr/share/GeographicLib/
+sudo rm -rf /tmp/egm96-5.tar.bz2
 
 echo "[postcreate] Installing workspace dependencies..."
 rosdep install --from-paths src --ignore-src -y \
